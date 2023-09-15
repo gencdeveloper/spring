ee23ac50fc87d223014713300b0b31538d1c5787
Emre <yunusozudogru34@gmail.com>
2023-09-16 01:13:41 +0200

    model

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/Spring08SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08SpringMvcApplication.class, args);
+	}
+
+}
