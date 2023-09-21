5d9ce74552b1dd3af4382d43d3e9f6407fbd52bf
Emre <yunusozudogru34@gmail.com>
2023-09-21 23:29:11 +0200

    added form

--- Spring-9-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-9-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,23 +3,26 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/student")//class level endPoint
 public class StudentController {
 
-    @RequestMapping("/register")
+   // @RequestMapping(value = "/register", method = RequestMethod.GET) //returning your endPoint
+    @GetMapping("/register")
     public String register(Model model){
 
     model.addAttribute("students", DataGenerator.createStudent());
 
 
-        return "student/register";
+        return "student/register"; //returning your html
     }
 
 
-    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
 
         return "student/welcome";
